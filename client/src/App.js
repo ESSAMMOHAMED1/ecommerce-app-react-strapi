2b34913f00a3454505ca7eeeb1511961bf522337
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-18 22:36:57 -0800

    add-routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { Route, BrowserRouter, Routes, useLocation } from "react-router-dom";
-
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails"
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   useEffect(() => {
@@ -13,6 +16,12 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="item/:itemId" element={<ItemDetails/>}/>
+          <Route path="checkout" element={<Checkout/>}/>
+          <Route path="checkout/success" element={<Confirmation/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
