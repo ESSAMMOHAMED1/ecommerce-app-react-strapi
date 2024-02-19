72e691c7acd9cd5ecc9154f72421b42eaa3ee192
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-18 22:09:03 -0800

    add-scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { Route, BrowserRouter, Routes, useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 function App() {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ScrollToTop />
+      </BrowserRouter>
     </div>
   );
 }
