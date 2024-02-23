f1bcb2f9427b73ab29a5ec124ac1359c40f24ece
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-23 02:02:05 -0800

    component-design-and-add-amount

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,17 +1,75 @@
-import React from 'react'
-import { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import {IconButton,Box,Typography,useTheme,Button} from "@mui/material"
-import AddIcon from "@mui/material/Add"
-import RemoveIcon from "@mui/icons-material/Remove"
-import { shades } from '../theme'
-import { addToCart } from '../state'
-import { useNavigate } from 'react-router-dom'
- 
-const Item = () => {
-  return (
-    <div>Item</div>
-  )
-}
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
 
-export default Item
+const Item = ({ item, width }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch;
+  const [count, setCount] = useState(1);
+  const [isHover, setIsHover] = useState(false);
+  const {
+    palette: { neutral },
+  } = useTheme();
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formats: {
+          medium: { url },
+        },
+      },
+    },
+  } = image;
+
+  return (
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}
+      >
+        <img
+          alt={item.name}
+          width="300px"
+          height="400px"
+          src={`http://localhost:1337${url}`}
+          onClick={() => navigate(`/item/${item.id}`)}
+          style={{ cursor: "pointer" }}
+        />
+        <Box
+          display={isHover ? "blocked" : "non"}
+          position="absolute"
+          bottom="10%"
+          left="0"
+          width="100%"
+          padding="0 5%"
+        >
+          <Box display="flex" justifyContent="space-between">
+            <Box
+              display="flex"
+              alignItems="center"
+              backgroundColor={shades.neutral[100]}
+              borderRadius="3px"
+            >
+              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography color={shades.primary[300]}>{count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Item;
