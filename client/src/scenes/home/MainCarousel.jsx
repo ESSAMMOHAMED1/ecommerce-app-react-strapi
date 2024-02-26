b6bcd93e9d4f573a509e9ca9b4a9b8c5c6ea239d
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-26 15:10:03 -0800

    import-all-images

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Box, IconButton, Typography, useMediaQuery } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import NavigateBefore from "@mui/icons-material/NavigateBefore"
-import NavigateNext from "@mui/icons-material/NavigateNext"
-import {shades} from "../../theme"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBefore from "@mui/icons-material/NavigateBefore";
+import NavigateNext from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+//import all images
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+
+  export const heroTextureImports = importAll(
+    require.context("../../assets",false,/\.(png|jpe?g|svg)$/)
+    
+  )
+
 const MainCarousel = () => {
   return <div>MainCarousel</div>;
 };
