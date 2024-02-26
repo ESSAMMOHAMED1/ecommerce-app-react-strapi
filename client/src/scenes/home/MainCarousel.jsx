205a003cac8b46a916bf8677308861121861baaf
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-26 14:58:14 -0800

    add-imports

---
+++ client/src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, IconButton, Typography, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import NavigateBefore from "@mui/icons-material/NavigateBefore"
+import NavigateNext from "@mui/icons-material/NavigateNext"
+import {shades} from "../../theme"
+const MainCarousel = () => {
+  return <div>MainCarousel</div>;
+};
+
+export default MainCarousel;
