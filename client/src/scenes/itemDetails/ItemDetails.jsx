d9aaced07f1f869f0520b0725f1cc6f2affe8d0a
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-09 00:42:38 -0700

    ge-item-img

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -47,7 +47,13 @@ const ItemDetails = () => {
       <Box flexWrap="wrap" columnGap="40px">
         {/* IMAGES */}
         <Box flex="1 1 40%" mb="40px">
-          <img alt={item?.name} />
+          <img
+            alt={item?.name}
+            width="100%"
+            height="100%"
+            src={`http://localhost1337${item?.attributes?.images?.data?.attributes?.formats?.medium?.url}`}
+            style={{ objectFit: "contain" }}
+          />
         </Box>
       </Box>
     </Box>
