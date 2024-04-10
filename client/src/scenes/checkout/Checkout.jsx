b18238a45f25dcfdf52d2ee1bd00e3beddddb793
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-09 22:58:14 -0700

    functions-and-states

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 import { useSelector } from "react-redux";
 import { Formik } from "formik";
 import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
 import * as yup from "yup";
 import { shades } from "../../theme";
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const isFirstStep = activeStep === 0;
+  const isSecondStep = activeStep === 1;
+  const handelFormSubmit = (value, action) => {
+    setActiveStep(activeStep + 1);
+  };
+  async function makePayment(values) {
+    
+  }
   return <Box></Box>;
 };
 
