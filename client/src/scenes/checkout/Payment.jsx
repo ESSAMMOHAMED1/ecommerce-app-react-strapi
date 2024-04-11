b41afb152fe13b3213a225ce5036407e4de7bfe8
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-11 03:55:14 -0700

    add-imports

---
+++ client/src/scenes/checkout/Payment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+const Payment = () => {
+  return (
+    <div>Payment</div>
+  )
+}
+
+export default Payment
