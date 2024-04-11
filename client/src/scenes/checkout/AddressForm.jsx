1123bb0ee387d54688ec94a020ab5b7815fe9215
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-11 02:32:15 -0700

    formattedError

--- client/src/scenes/checkout/AddressForm.jsx
+++ client/src/scenes/checkout/AddressForm.jsx
@@ -11,8 +11,13 @@ const AddressForm = ({
   handleBlur,
   handleChange,
 }) => {
-    const isNonMobile = useMediaQuery("(min-width:600px)");
-    // these functions allow for better code readability
-    const formattedName = (field) => `${type}.${field}`;
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  // these functions allow for better code readability
+  const formattedName = (field) => `${type}.${field}`;
+  const formattedError = (field) =>
+    Boolean(
+      getIn(touched, formattedName(field)) &&
+        getIn(errors, formattedName(field))
+    );
 };
 export default AddressForm;
