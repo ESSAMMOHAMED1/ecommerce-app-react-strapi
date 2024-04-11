7c81f168911be12a4e5628b8118a8e611c8d3b76
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-11 04:59:52 -0700

    retrieve-item-information

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -7,4 +7,26 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order");
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+      // retrieve item information
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+    }
